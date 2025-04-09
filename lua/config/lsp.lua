8b5c0c0a83a0f4a971197040062f2a2f772c6ddd
Vinicius Mayrink <vmayrink12@gmail.com>
2025-04-08 21:10:29 -0300

    feat(lsp): adds icons for diagnostics sign column symbols

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,2 +1,7 @@
 -- INFO: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs
 vim.lsp.enable({ "lua_ls", "gopls" })
+
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
