d1b4f1ecfd3276ecf600854ae3c47051e5798b6c
Vinicius Mayrink <vmayrink12@gmail.com>
2025-04-11 22:38:48 -0300

    chore(lsp): uses the standard way of defining signs as the previous was deprecated

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,12 +1,13 @@
 -- INFO: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs
 vim.lsp.enable({ "lua_ls", "gopls", "intelephense" })
 
-vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
-
 vim.diagnostic.config({
   virtual_text = true,
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
+  },
 })
