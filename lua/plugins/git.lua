6f25cd1f8113910c2dc58ecf81fd192d0eb46547
Vinicius Mayrink <vmayrink12@gmail.com>
2025-07-05 19:38:57 -0300

    feat(git): adds mini diff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -61,5 +61,15 @@ return {
     },
   },
 
+  {
+    "echasnovski/mini.diff",
+    config = function()
+      local diff = require("mini.diff")
+      diff.setup({
+        source = diff.gen_source.none(),
+      })
+    end,
+  },
+
   { "rhysd/git-messenger.vim" },
 }
