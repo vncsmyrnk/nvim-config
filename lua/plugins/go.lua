71e4772c8eee07b04726089d75fc3241a8c284f1
Vinicius Mayrink <vmayrink12@gmail.com>
2025-07-12 14:39:16 -0300

    chore(go): lazy loads vim go

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -7,12 +7,7 @@ return {
 
   {
     "fatih/vim-go",
-    lazy = false,
+    ft = { "go", "gomod" },
     build = ":GoInstallBinaries",
-    keys = {
-      { "<leader>cgF", "<cmd>GoTestFile<cr>", desc = "Go: Test file" },
-      { "<leader>cgf", "<cmd>GoTestFunc<cr>", desc = "Go: Test function" },
-      { "<leader>cgc", "<cmd>GoCoverage<cr>", desc = "Go: Coverage" },
-    },
   },
 }
