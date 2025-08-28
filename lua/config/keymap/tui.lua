c29b08ecefc035a88c8896a2472af5932d690934
Vinicius Mayrink <vmayrink12@gmail.com>
2025-08-27 22:07:20 -0300

    feat(keymap/tui): adds gemini keymap

--- lua/config/keymap/tui.lua
+++ lua/config/keymap/tui.lua
@@ -1,3 +1,4 @@
 local utils = require("config.utils")
 
 vim.keymap.set("n", "<leader>gl", utils.on_a_new_tab("lazygit"), { desc = "TUI: Opens lazygit" })
+vim.keymap.set("n", "<leader>ag", utils.on_a_new_tab("gemini"), { desc = "TUI: Opens gemini" })
