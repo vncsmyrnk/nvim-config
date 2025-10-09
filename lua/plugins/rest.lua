1567917821e06d8d3c2d9a3aaf313a3a176d5e60
Vinicius Mayrink <vmayrink12@gmail.com>
2025-10-09 16:40:07 -0300

    fix(plugins/rest): removes custom aliases as the default ones are the same
    This also avoids the keymaps being activated on non-http files.

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -2,11 +2,6 @@ return {
   -- INFO: https://neovim.getkulala.net/
   {
     "mistweaverco/kulala.nvim",
-    keys = {
-      { "<leader>rs", desc = "Send request" },
-      { "<leader>ra", desc = "Send all requests" },
-      { "<leader>rb", desc = "Open scratchpad" },
-    },
     ft = { "http", "rest" },
     opts = {
       kulala_keymaps = {
