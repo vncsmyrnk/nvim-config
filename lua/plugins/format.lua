b94f509bb09c1bd115772fee42928ef3b0ea2bed
Vinicius Mayrink <vmayrink12@gmail.com>
2025-10-11 19:55:21 -0300

    chore(plugins/format): formats sh buffers with `shfmt`

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -16,6 +16,7 @@ return {
         lua = { "stylua" },
         json = { "jq" },
         zsh = { "shfmt" },
+        sh = { "shfmt" },
         rust = { "rustfmt" },
         nix = { "nixfmt" },
       },
