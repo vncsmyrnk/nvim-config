8fb13f141a9bd1681d2b3c23f9c8ac2ceabdb9de
Vinicius Mayrink <vmayrink12@gmail.com>
2026-04-04 17:48:57 -0300

    chore(keymaps/tab): adds mappings for switching to the first, last and last accessed tabs

--- lua/config/keymap/tab.lua
+++ lua/config/keymap/tab.lua
@@ -1,7 +1,30 @@
 local set = vim.keymap.set
 
 set("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "Opens a new tab" })
-set("n", "<leader>tt", "<cmd>tab term<cr>", { desc = "Opens a new tab in terminal mode" })
-set("n", "<leader>th", "<cmd>tabmove -1<cr>", { desc = "Moves tab to the right" })
-set("n", "<leader>tl", "<cmd>tabmove +1<cr>", { desc = "Moves tab to the left" })
+set("n", "<leader>t$", "<cmd>tabnext $<cr>", { desc = "Opens the last tab" })
+set("n", "<leader>t0", "<cmd>tabnext 1<cr>", { desc = "Opens the first tab" })
+set(
+  "n",
+  "<leader>t<tab>",
+  "<cmd>tabnext #<cr>",
+  { desc = "Opens the last accessed tab" }
+)
+set(
+  "n",
+  "<leader>tt",
+  "<cmd>tab term<cr>",
+  { desc = "Opens a new tab in terminal mode" }
+)
+set(
+  "n",
+  "<leader>th",
+  "<cmd>tabmove -1<cr>",
+  { desc = "Moves tab to the right" }
+)
+set(
+  "n",
+  "<leader>tl",
+  "<cmd>tabmove +1<cr>",
+  { desc = "Moves tab to the left" }
+)
 set("n", "<leader>tq", "<cmd>tabclose<cr>", { desc = "Closes current tab" })
