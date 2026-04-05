d984faf02a42b345d35c5c7e24c30e5d1f17636d
Vinicius Mayrink <vmayrink12@gmail.com>
2026-04-05 15:23:58 -0300

    chore(plugins/repl): moves keymaps to lazy `keys`

--- lua/plugins/repl.lua
+++ lua/plugins/repl.lua
@@ -16,18 +16,40 @@ return {
         repl_open_cmd = "vertical botright 80 split",
       },
       keymaps = {
-        toggle_repl = "<space>pt",
-        restart_repl = "<space>pr",
-        send_motion = "<space>pc",
-        visual_send = "<A-enter>",
-        send_file = "<A-enter>",
-        send_line = "<A-;>",
         clear = "<space>pl",
       },
     },
     keys = {
       { "<leader>po", "<cmd>IronRepl<cr>", desc = "Repl: open UI" },
       { "<leader>pq", "<cmd>IronHide<cr>", desc = "Repl: hide UI" },
+      {
+        "<leader>pr",
+        function()
+          require("iron.core").repl_restart()
+        end,
+        desc = "Repl: restart",
+      },
+      {
+        "<A-enter>",
+        mode = { "n", "v" },
+        function()
+          local iron = require("iron.core")
+          if vim.api.nvim_get_mode().mode == "V" then
+            iron.mark_visual()
+            iron.send_mark()
+            return
+          end
+          iron.send_file()
+        end,
+        desc = "Repl: send",
+      },
+      {
+        "<A-;>",
+        function()
+          require("iron.core").send_line()
+        end,
+        desc = "Repl: send line",
+      },
     },
   },
 }
